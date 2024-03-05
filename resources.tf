9d8d08d2b8950d87e436e7f5ac15a09378c08bdf
dinilv <dinil@github.com>
2024-03-05 20:53:38 +0530

    fix: terraform validate check

--- resources.tf
+++ resources.tf
@@ -1,9 +1,15 @@
-resource "google_project" "gcpa" {
-  name       = "gcpa"
+data "google_project" "existing" {
+  name = "gcpa"
   project_id = "gcpa-415907"
 }
 
+resource "google_project" "gcpa" {
+  count = data.google_project.existing ? 0 : 1
+  name       = count ? "gcpa" : null 
+  project_id = count ? "gcpa-415907": null
+}
+
 resource "google_app_engine_application" "quote_daily_app" {
-  project     = google_project.gcpa.project_id
+  project     = data.gcpa.project_id
   location_id = "us-central"
 }
