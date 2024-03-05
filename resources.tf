77913e5888e82b1c7811200559cc78c6404f9b38
dinilv <dinil@github.com>
2024-03-05 21:14:41 +0530

    fix: create gae

--- resources.tf
+++ resources.tf
@@ -1,15 +1,8 @@
 data "google_project" "existing" {
-  name = "gcpa"
   project_id = "gcpa-415907"
 }
 
-resource "google_project" "gcpa" {
-  count = data.google_project.existing ? 0 : 1
-  name       = count ? "gcpa" : null 
-  project_id = count ? "gcpa-415907": null
-}
-
 resource "google_app_engine_application" "quote_daily_app" {
-  project     = data.gcpa.project_id
+  project     = data.google_project.existing.project_id
   location_id = "us-central"
 }
