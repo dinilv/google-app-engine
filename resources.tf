af7a469b9a646a6655f419741fd8881410d95ea6
dinilv <dinil@github.com>
2024-03-27 14:13:31 +0530

    tf: lifecycle

--- resources.tf
+++ resources.tf
@@ -1,20 +1,24 @@
 data "google_project" "existing" {
-  project_id = "gcpa-415907"
+ project_id = "gcpa-415907"
 }
 
-
 resource "google_app_engine_application" "quote_daily_app" {
-  project     = data.google_project.existing.project_id
-  location_id = "us-central1" 
-  lifecycle {
+ project     = data.google_project.existing.project_id
+ location_id = "us-central1"
+
+ lifecycle {
     create_before_destroy = true
  }
 }
 
 resource "google_artifact_registry_repository" "docker_images" {
-  project       = data.google_project.existing.project_id
-  location      = "us-central1" 
-  repository_id = "app-engine"
-  description   = "example docker repository"
-  format        = "DOCKER"
+ project       = data.google_project.existing.project_id
+ location      = "us-central1"
+ repository_id = "app-engine"
+ description   = "example docker repository"
+ format        = "DOCKER"
+
+ lifecycle {
+    create_before_destroy = true
+ }
 }
