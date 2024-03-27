666ceea2b61155bee524d4549664b00aa62507e0
dinilv <dinil@github.com>
2024-03-27 13:34:30 +0530

    terraform: fmt check

--- resources.tf
+++ resources.tf
@@ -1,16 +1,16 @@
 data "google_project" "existing" {
- project_id = "gcpa-415907"
+  project_id = "gcpa-415907"
 }
 
 
 resource "google_app_engine_application" "quote_daily_app" {
- project     = data.google_project.existing.project_id
- location_id = "us-central1" # Use the region directly here
+  project     = data.google_project.existing.project_id
+  location_id = "us-central1" # Use the region directly here
 }
 
 resource "google_artifact_registry_repository" "docker_images" {
- location      = "us-central1" # And here
- repository_id = "app-engine"
- description   = "example docker repository"
- format        = "DOCKER"
+  location      = "us-central1" # And here
+  repository_id = "app-engine"
+  description   = "example docker repository"
+  format        = "DOCKER"
 }
