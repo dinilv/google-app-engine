7a7502c3d1fb253c71f6044b948ec0ccd99a542b
dinilv <dinil@github.com>
2024-03-27 22:11:27 +0530

    test: terraform issues

--- resources.tf
+++ resources.tf
@@ -2,11 +2,6 @@ data "google_project" "existing" {
  project_id = "gcpa-415907"
 }
 
-resource "google_app_engine_application" "quote_daily" {
- project     = data.google_project.existing.project_id
- location_id = "us-central1"
-}
-
 resource "google_artifact_registry_repository" "docker_images" {
  project       = data.google_project.existing.project_id
  location      = "us-central1"
