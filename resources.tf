c71d134ff59a8bb442ffcbc13b15fcd434f7a151
dinilv <dinil@github.com>
2024-03-27 13:49:43 +0530

    terraform: lifecycle added

--- resources.tf
+++ resources.tf
@@ -5,12 +5,15 @@ data "google_project" "existing" {
 
 resource "google_app_engine_application" "quote_daily_app" {
   project     = data.google_project.existing.project_id
-  location_id = "us-central1" # Use the region directly here
+  location_id = "us-central1" 
+  lifecycle {
+    create_before_destroy = true
+ }
 }
 
 resource "google_artifact_registry_repository" "docker_images" {
   project       = data.google_project.existing.project_id
-  location      = "us-central1" # And here
+  location      = "us-central1" 
   repository_id = "app-engine"
   description   = "example docker repository"
   format        = "DOCKER"
